6f8586c6fe2d033f007e9a14483576de48a05d46
John Ospina <jhonospina150@gmail.com>
2024-09-13 09:15:13 -0500

    add horys life cycle

---
+++ core/src/androidMain/kotlin/com/apptank/horus/client/HorusActivityLifeCycle.kt
@@ -0,0 +1,30 @@
+package com.apptank.horus.client
+
+import android.app.Activity
+import android.app.Application.ActivityLifecycleCallbacks
+import android.os.Bundle
+import com.apptank.horus.client.lifecycle.HorusLifeCycle
+
+class HorusActivityLifeCycle : ActivityLifecycleCallbacks {
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        HorusLifeCycle.onCreate()
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        HorusLifeCycle.onResume()
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        HorusLifeCycle.onPause()
+    }
+
+    override fun onActivityStarted(activity: Activity) = Unit
+
+    override fun onActivityStopped(activity: Activity) = Unit
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+
+    override fun onActivityDestroyed(activity: Activity) = Unit
+
+}
--- core/src/commonMain/kotlin/com/apptank/horus/client/lifecycle/HorusLifeCycle.kt
+++ core/src/commonMain/kotlin/com/apptank/horus/client/lifecycle/HorusLifeCycle.kt
@@ -1,9 +1,7 @@
-package com.apptank.horus.client
+package com.apptank.horus.client.lifecycle
 
-import android.app.Activity
-import android.app.Application.ActivityLifecycleCallbacks
-import android.os.Bundle
 import com.apptank.horus.client.di.HorusContainer
+import com.apptank.horus.client.di.ILifeCycle
 import com.apptank.horus.client.di.INetworkValidator
 import com.apptank.horus.client.eventbus.CallbackEvent
 import com.apptank.horus.client.eventbus.EventBus
@@ -11,7 +9,8 @@ import com.apptank.horus.client.eventbus.EventType
 import com.apptank.horus.client.sync.manager.RemoteSynchronizatorManager
 import com.apptank.horus.client.tasks.ControlTaskManager
 
-class HorusSynchronizeHandlerActivityLifeCycle : ActivityLifecycleCallbacks {
+
+object HorusLifeCycle: ILifeCycle {
 
     private var remoteSynchronizatorManager: RemoteSynchronizatorManager? = null
 
@@ -21,30 +20,21 @@ class HorusSynchronizeHandlerActivityLifeCycle : ActivityLifecycleCallbacks {
 
     private val networkValidator: INetworkValidator by lazy { HorusContainer.getNetworkValidator() }
 
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+    override fun onCreate() {
         remoteSynchronizatorManager = HorusContainer.createRemoteSynchronizatorManager().also {
             it.trySynchronizeData()
         }
     }
 
-    override fun onActivityStarted(activity: Activity) {
+    override fun onResume() {
+        EventBus.register(EventType.ACTION_CREATED, callbackEventActionCreated)
+        networkValidator.registerNetworkCallback()
         ControlTaskManager.start()
     }
 
-    override fun onActivityResumed(activity: Activity) {
-        EventBus.register(EventType.ACTION_CREATED, callbackEventActionCreated)
-        networkValidator.registerNetworkCallback()
-    }
-
-    override fun onActivityPaused(activity: Activity) {
+    override fun onPause() {
         EventBus.unregister(EventType.ACTION_CREATED, callbackEventActionCreated)
         networkValidator.unregisterNetworkCallback()
     }
-
-    override fun onActivityStopped(activity: Activity) = Unit
-
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
-
-    override fun onActivityDestroyed(activity: Activity) = Unit
 
 }
